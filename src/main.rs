087f0554576d877133513272814de7178e86fc35
Kate the Gay <kanako@gayest.dev>
2025-07-20 23:31:07 -0700

    Add color-eyre

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use color_eyre::eyre::Result;
 use env_logger::Env;
 use log::info;
 use sqlx::{Connection, SqliteConnection};
@@ -11,20 +12,23 @@ mod load;
 mod models;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    color_eyre::install()?;
 
-    let mut conn = SqliteConnection::connect("sqlite://test.db").await.unwrap();
+    let mut conn = SqliteConnection::connect("sqlite://test.db").await?;
 
-    sqlx::migrate!().run(&mut conn).await.unwrap();
+    sqlx::migrate!().run(&mut conn).await?;
 
-    let genius = genius::Genius::new(&env::var("GENIUS_ACCESS_TOKEN").unwrap());
+    let genius = genius::Genius::new(&env::var("GENIUS_ACCESS_TOKEN")?);
 
     let artists = &["Mili (JPN)", "AWAAWA"];
 
-    if !is_initialized(&mut conn, artists).await.unwrap() {
+    if !is_initialized(&mut conn, artists).await? {
         info!("loading lyrics");
 
-        load_lyrics(&mut conn, &genius, artists).await.unwrap();
+        load_lyrics(&mut conn, &genius, artists).await?;
     }
+
+    Ok(())
 }
